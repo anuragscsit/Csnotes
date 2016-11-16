cf3f6012fe1dee9fd0f4353b5d21cb07022817ad
Anurag <anumfp@gmail.com>
2016-11-16 18:09:47 +0530

    redirection of confirmation mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
    config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'csnotes.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = true
